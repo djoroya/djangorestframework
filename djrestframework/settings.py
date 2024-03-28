83e9acf7fbd75852666192b2c30e4185ccd07cf7
Jesus Oroya <41575404+djoroya@users.noreply.github.com>
2024-03-28 16:09:32 +0100

    add render settings

--- djrestframework/settings.py
+++ djrestframework/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,9 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-%r^fvx27da9y%!9%6l)nbwgm3#=r!e1^lbcb3)(5u0mt@rfd0!'
+SECRET_KEY = os.environ.get('SECRET_KEY',default="You forgot to set SECRET_KEY environment variable")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
